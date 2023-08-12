1a66d4be40040143eb3c857cf4c89b8a9e05047e
KaiKai <kaikai_miniworld@outlook.com>
2023-08-12 23:50:30 +0800

    fix: typo (#9)

--- examples/quick-starter.ts
+++ examples/quick-starter.ts
@@ -3,4 +3,4 @@ import { Bot } from 'fanbook-api-node-sdk';
 const YOUR_BOT_TOKEN = '在此填入你的机器人令牌';
 
 const bot = new Bot(YOUR_BOT_TOKEN);
-console.log('Infomation about you bot:', await bot.getMe());
+console.log('Infomation about your bot:', await bot.getMe());
