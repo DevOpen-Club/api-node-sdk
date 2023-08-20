a01b261982e1e8b8003eaf853df50be642d11208
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-08-20 23:49:39 +0800

    docs: 生成 API 文档时移除 `/api/README.md`

--- scripts/doc-gen.ts
+++ scripts/doc-gen.ts
@@ -1,5 +1,6 @@
 import * as typedoc from 'typedoc';
 import * as path from 'path';
+import { rmSync } from 'fs';
 
 const OUT_DIR = path.resolve(__dirname, '../docs/api');
 
@@ -17,4 +18,5 @@ const OUT_DIR = path.resolve(__dirname, '../docs/api');
   const project = app.convert();
   if (!project) throw new Error('生成失败');
   await app.generateDocs(project, OUT_DIR);
+  rmSync(path.resolve(OUT_DIR, 'README.md')); // `/api/README.md` 无用，并且存在 dead link
 })();
