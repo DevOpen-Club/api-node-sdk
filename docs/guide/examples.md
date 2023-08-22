4257db9059696f632d1b2185b36a13821eb189c9
KaiKai <kaikai_miniworld@outlook.com>
2023-08-23 01:59:46 +0800

    docs: 添加事件订阅示例；修改现有示例和文档 (#24)

---
+++ docs/guide/examples.md
@@ -0,0 +1,9 @@
+# 示例
+
+## 获取机器人信息
+
+<<< @/../examples/quick-starter.ts
+
+## 订阅事件
+
+<<< @/../examples/listen-events.ts
--- examples/quick-starter.ts
+++ examples/quick-starter.ts
@@ -1,6 +1,10 @@
+/**
+ * 获取机器人基本信息。
+ */
+
 import { Bot } from 'fanbook-api-node-sdk';
 
 const YOUR_BOT_TOKEN = '在此填入你的机器人令牌';
 
 const bot = new Bot(YOUR_BOT_TOKEN);
-console.log('Infomation about your bot:', await bot.getMe());
+console.log('Information about your bot:', await bot.getMe());
