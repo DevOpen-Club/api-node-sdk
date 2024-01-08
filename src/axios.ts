6cbf6e14b943a844628ec90400bfffa5ccc7d915
kaikaibenkai <kaikai_miniworld@outlook.com>
2024-01-08 11:29:09 +0800

    fix: 修复 bigint 解析错误；添加测试

--- src/axios.ts
+++ src/axios.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosResponse, CreateAxiosDefaults } from 'axios';
+import axios, { AxiosResponse, CreateAxiosDefaults, InternalAxiosRequestConfig } from 'axios';
+import { isPlainObject } from 'is-plain-object';
 import jsonBigint from 'json-bigint';
 import { FanbookApiError } from './error';
 
@@ -7,21 +8,40 @@ export const bigintJsonParser = jsonBigint({
   useNativeBigInt: true,
 });
 
+/** 测试 Content-Type 是否为 json 的正则表达式。 */
+const isJsonContentType = /^application\/(.*\+)?json$/;
 /**
- * 转换请求响应。
- * @param data 响应体
- * @returns 转换后的响应体
+ * 解析请求 bigint。
+ * @param req 请求数据
+ * @returns 转换后的请求数据
  */
-function parseResponseBigint(data: unknown) {
-  console.log(data);
-  if (typeof data === 'string') {
-    try { // 能转 JSON 就转
-      return bigintJsonParser.parse(data);
-    } catch { // 不能转原样返回
-      return data;
-    }
+function requestBigintInterceptor(req: InternalAxiosRequestConfig) {
+  const type = req.headers['content-type'];
+  if (
+    (typeof type === 'string' && isJsonContentType.test(type))
+    || isPlainObject(req.data)
+  ) { // 是 JSON
+    req.data = bigintJsonParser.stringify(req.data);
+    req.headers['content-type'] ??= 'application/json';
   }
-  return data;
+  return req;
+}
+/**
+ * 解析响应 bigint。
+ * @param res 响应数据
+ * @returns 转换后的响应数据
+ */
+function responseBigintInterceptor(res: AxiosResponse) {
+  // 非 json 不处理
+  const type = res.headers['content-type'];
+  if (typeof type !== 'string') return res;
+  if (!isJsonContentType.test(type)) return res;
+
+  // 带 bigint 解析 json，不行就算了
+  try {
+    res.data = bigintJsonParser.parse(res.data);
+  } catch {}
+  return res;
 }
 
 /**
@@ -30,13 +50,17 @@ function parseResponseBigint(data: unknown) {
  * @returns Axios 实例
  */
 export function createAxios(options?: CreateAxiosDefaults) {
-  let pipes = options?.transformResponse;
-  // 将 pipes 转数组，然后浅拷贝，保证后续修改 options 不会影响到 pipes
-  if (Array.isArray(pipes)) pipes = [...pipes];
-  else pipes = pipes ? [pipes] : []; // pipes 可能为 undefined
-
-  pipes.unshift(parseResponseBigint);
-  return axios.create(options);
+  const inst = axios.create({
+    ...options,
+    // 由于 axios 自动解析 json 导致 bigint 精度丢失，要阻止它的 json 解析
+    // 如果用户已经给定 pipes，它会阻止 json 解析
+    // 如果用户没有给定 pipes，要用 `(x) => x` 阻止 json 解析
+    transformRequest: options?.transformRequest ?? ((x) => x),
+    transformResponse: options?.transformResponse ?? ((x) => x),
+  });
+  inst.interceptors.request.use(requestBigintInterceptor);
+  inst.interceptors.response.use(responseBigintInterceptor);
+  return inst;
 }
 
 /**
